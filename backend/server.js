0d13ab11959215b94dc1059fc6318485fe1617e9
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-09-24 14:09:29 +0530

    added middleware to protect the send message route

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -8,8 +10,11 @@ const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
-app.use(express.json()) // to parse the incoming requests with JSON payloads(from req.body)
+app.use(express.json()); // to parse the incoming requests with JSON payloads(from req.body)
+app.use(cookieParser()); // to parse the incoming cookies from req.cookies
+
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(5000, () => {
   connectToMongoDB();
