520162d2633a0551fb8987aa92736ee3d8ffd98e
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-09-25 13:37:35 +0530

    Imported react-router-dom and set path for each pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css'
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
@@ -5,10 +6,15 @@ import SignUp from './pages/signup/SignUp';
 
 function App() {
 
-  return <div className='p-4 h-screen flex items-center justify-center'>
-    <Home/>
-
-  </div>;
+  return (
+    <div className="p-4 h-screen flex items-center justify-center">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App
