070605a8617e9ec15c3b885034a1e5d9696c8ac8
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-09-27 15:13:32 +0530

    Adding auto scroll feature whenever a new chat found

--- frontend/src/components/messages/Messages.tsx
+++ frontend/src/components/messages/Messages.tsx
@@ -1,22 +1,34 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Message from "./Message";
 import useGetMessages from "../../hooks/useGetMessages";
 import MessageSkeleton from "../skeletons/MessageSkeleton";
 
 const Messages = () => {
   const { messages, loading } = useGetMessages();
-  console.log("messages:****", messages);  
+
+  const endOfMessagesRef = useRef(null);
+  useEffect(() => {
+    if (endOfMessagesRef.current) {
+      //@ts-ignore
+      endOfMessagesRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [messages]);
+
   return (
     <div className="px-4 flex-1 overflow-auto">
-      {!loading && messages.length>0 && messages.map((message:any)=><Message key={message._id} message={message}/>)}
+      {!loading &&
+        messages.length > 0 &&
+        messages.map((message: any) => (
+          <Message key={message._id} message={message} />
+        ))}
       {loading &&
         [...Array(3)].map((_, idx) => (
           <MessageSkeleton key={idx}></MessageSkeleton>
         ))}
-
       {!loading && messages.length === 0 && (
         <p className="text-center">Yay! Send a message to connect...</p>
       )}
+      <div ref={endOfMessagesRef} /> {/* Empty div to scroll to */}
     </div>
   );
 };
