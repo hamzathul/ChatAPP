4e87d755316adeae4ede0ee4cd4e9acc057c5a1e
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-09-27 11:36:42 +0530

    working on message component

--- frontend/src/components/messages/Message.tsx
+++ frontend/src/components/messages/Message.tsx
@@ -1,18 +1,27 @@
 import React from 'react'
+import { useAuthContext } from '../../context/AuthContext';
+import useConversation from '../../zustand/useConversation';
 
-const Message = () => {
+const Message = ({message}) => {
+  const {authUser} = useAuthContext()
+  const {selectedConversation}  = useConversation()
+
+  const fromMe = message.senderId = authUser._id
+  const chatClassName = fromMe ? 'chat-end' : 'chat-start'
+  const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic
+  const bubbleBgColor = fromMe ? 'bg-orange-400': ""
+
   return (
-    <div className="chat chat-end">
+    <div className={`chat ${chatClassName}`}>
       <div className="chat-image avatar">
         <div className="w-10 rounded-full">
-          <img
-            alt="Tailwind CSS chat bubble component"
-            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-          />
+          <img alt="Tailwind CSS chat bubble component" src={profilePic} />
         </div>
       </div>
-      <div className='chat-bubble text-white bg-orange-400'>Hello</div>
-      <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>12:22</div>
+      <div className={`chat-bubble text-white bg-orange-400 ${bubbleBgColor}`}>Hello</div>
+      <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
+        12:22
+      </div>
     </div>
   );
 }
--- frontend/src/components/messages/Messages.tsx
+++ frontend/src/components/messages/Messages.tsx
@@ -1,23 +1,27 @@
-import React from 'react'
-import Message from './Message';
-import useGetMessages from '../../hooks/useGetMessages';
-import MessageSkeleton from '../skeletons/MessageSkeleton';
+import React from "react";
+import Message from "./Message";
+import useGetMessages from "../../hooks/useGetMessages";
+import MessageSkeleton from "../skeletons/MessageSkeleton";
 
 const Messages = () => {
-  const {messages, loading} = useGetMessages()
-  console.log("messages:" , messages)
+  const { messages, loading } = useGetMessages();
+  console.log("messages:", messages);
   return (
     <div className="px-4 flex-1 overflow-auto">
-     {loading && [...Array(3)].map((_,idx)=><MessageSkeleton key={idx}></MessageSkeleton>)}
+      {!loading && messages.length>0 && messages.map((message:any)=><Message key={message._id} message={message}/>)}
+      {loading &&
+        [...Array(3)].map((_, idx) => (
+          <MessageSkeleton key={idx}></MessageSkeleton>
+        ))}
 
-     {!loading && messages.length === 0 && (<p className='text-center'>Yay! Send a message to connect...</p>)}
+      {!loading && messages.length === 0 && (
+        <p className="text-center">Yay! Send a message to connect...</p>
+      )}
     </div>
   );
-}
+};
 
-export default Messages
-
-
+export default Messages;
 
 //STARTER CODE
 // import React from "react";
