8c174fafb9d715c8cb8ae8dbbe3ef281aedf94f1
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-09-28 13:43:58 +0530

    completed basic implementation of socket.io in client and server side

--- frontend/src/context/SocketContext.tsx
+++ frontend/src/context/SocketContext.tsx
@@ -1,34 +1,46 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { useAuthContext } from "./AuthContext";
-import io from 'socket.io-client'
+import io from "socket.io-client";
 //@ts-ignore
-export const SocketContext = createContext();
+const SocketContext = createContext();
+
+export const useSocketContext = () => {
+  return useContext(SocketContext); //last written code
+};
+
 //@ts-ignore
 export const SocketContextProvider = ({ children }) => {
-    const[socket, setSocket] = useState(null)
-    const [onlineUsers, setOnlineUsers] = useState([])
-    //@ts-ignore
-    const {authUser} = useAuthContext()
-    //@ts-ignore
-    useEffect(()=>{
-        if(authUser){
-            const socket = io("http://localhost:5000",{
-                query:{
-                    userId:authUser._id
-                }
-            })
-            //@ts-ignore
-            setSocket(socket)
-            
-            return ()=> socket.close()
-        } else{
-            if(socket){
-                //@ts-ignore
-                socket.close()
-                setSocket(null)
-            }
-        }
+  const [socket, setSocket] = useState(null);
+  const [onlineUsers, setOnlineUsers] = useState([]);
+  //@ts-ignore
+  const { authUser } = useAuthContext();
+  //@ts-ignore
+  useEffect(() => {
+    if (authUser) {
+      const socket = io("http://localhost:5000", {
+        query: {
+          userId: authUser._id,
+        },
+      });
+      //@ts-ignore
+      setSocket(socket);
+      //socket.on() is used to listen to the events. can be used both client and server side
+      socket.on("getOnlineUsers", (users) => {
+        setOnlineUsers(users);
+      });
 
-    },[])
-  return <SocketContext.Provider value={{socket, onlineUsers}}>{children}</SocketContext.Provider>;
+      return () => socket.close();
+    } else {
+      if (socket) {
+        //@ts-ignore
+        socket.close();
+        setSocket(null);
+      }
+    }
+  }, []);
+  return (
+    <SocketContext.Provider value={{ socket, onlineUsers }}>
+      {children}
+    </SocketContext.Provider>
+  );
 };
